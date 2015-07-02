95b67313d604f5cca2b04f68d7afe71199e5b475
maydaycha <maydaychaaaa@gmail.com>
2015-07-02 21:05:46 +0800

    handle exception globally

--- fb.py
+++ fb.py
@@ -8,6 +8,7 @@ import json
 import unicodecsv as csv
 import sys
 import urllib3
+import tablib
 
 # disable ssl sucurity warning
 urllib3.disable_warnings()
@@ -24,12 +25,9 @@ access_token = r.text.split('=')[1]
 graph = facebook.GraphAPI(access_token=access_token, version='2.2')
 
 
-# post = graph.get_object(id="283092135094363")
-# print json.dumps(post, ensure_ascii=False)
 
-
-_fan_page_id = "283092135094363"
-# fan_page_id = "652438848137404"
+# _fan_page_id = "283092135094363"
+_fan_page_id = "652438848137404"
 _feeds = graph.get_connections(id=_fan_page_id, connection_name='feed')
 # previous_url = feeds['paging']['previous']
 
@@ -37,125 +35,157 @@ _feeds = graph.get_connections(id=_fan_page_id, connection_name='feed')
 def get_comments(feeds, file_name):
 
     fieldnames = ['commenter_id', 'commenter_name', 'commenter_message', 'commenter_message_created_time', 'replier_id', 'replier_name', 'replier_message', 'replier_message_created_time']
+    headers = ('commenter_id', 'commenter_name', 'commenter_message', 'commenter_message_created_time', 'replier_id', 'replier_name', 'replier_message', 'replier_message_created_time')
 
-    # 針對每一則貼文
-    for ele in feeds['data']:
+    try:
+        # 針對每一則貼文
+        for ele in feeds['data']:
 
-        # break
+            # break
 
-        csv_file = open(ele['id'].split('_')[1] + '.csv', 'w')
-        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        csv_writer.writeheader()
+            csv_file = open(ele['id'].split('_')[1] + '.xls', 'wb')
+            # csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            # csv_writer.writeheader()
 
-        # 每一則貼文中的留言
-        next_comments_url = ""
+            data = []
 
-        is_first = True
+            # 每一則貼文中的留言
+            next_comments_url = ""
 
-        if 'comments' in ele:
-            print "Got comment!"
-            # need to get reply if exist
-            print "@@@@@@@@@@@@@@@@"
-            print json.dumps(ele, ensure_ascii=False)
-            print "@@@@@@@@@@@@@@@@"
-            if 'next' in ele['comments']['paging']:
-                next_comments_url = ele['comments']['paging']['next']
-            else:
-                next_comments_url = ''
+            is_first = True
 
-            print "next_comments_url: %s" % next_comments_url
-
-            while True:
-                if is_first:
-                    comments = ele['comments']['data']
-                    is_first = False
+            if 'comments' in ele:
+                print "Got comment!"
+                # need to get reply if exist
+                print "@@@@@@@@@@@@@@@@"
+                print json.dumps(ele, ensure_ascii=False)
+                print "@@@@@@@@@@@@@@@@"
+                if 'next' in ele['comments']['paging']:
+                    next_comments_url = ele['comments']['paging']['next']
                 else:
-                    if next_comments_url != '':
-                        r = requests.get(next_url)
-                        json_obj = json.loads(r.text)
-                        comments = json_obj['data']
-                        next_comments_url = json_obj['paging']['next']
+                    next_comments_url = ''
 
-                for comment in comments:
-                    # print json.dumps(comment, ensure_ascii=False)
+                print "next_comments_url: %s" % next_comments_url
 
-                    # 寫入 csv
-                    # commenter_name = unicode(json.dumps(comment['from']['name'], ensure_ascii=False)).encode('UTF-8')
-                    commenter_name = json.dumps(comment['from']['name'], ensure_ascii=False)
-                    # self.writer.writerow([unicode(s).encode("utf-8") for s in row])
-                    print 'commenter_name: %s' % commenter_name
-                    csv_writer.writerow({'commenter_id': '', 'commenter_name': commenter_name, 'commenter_message': '', 'commenter_message_created_time': '', 'replier_id': '', 'replier_name': '', 'replier_message': '', 'replier_message_created_time': ''})
+                while True:
+                    if is_first:
+                        comments = ele['comments']['data']
+                        is_first = False
+                    else:
+                        if next_comments_url != '':
+                            r = requests.get(next_comments_url)
+                            json_obj = json.loads(r.text)
+                            comments = json_obj['data']
+
+                            if 'next' in json_obj['paging']:
+                                next_comments_url = json_obj['paging']['next']
+                            else:
+                                next_comments_url = ''
+
+                    for comment in comments:
+                        # print json.dumps(comment, ensure_ascii=False)
+
+                        # 寫入 csv
+                        commenter_name = json.dumps(comment['from']['name'], ensure_ascii=False)
+                        commenter_id = json.dumps(comment['from']['id'], ensure_ascii=False)
+                        commenter_message = json.dumps(comment['message'], ensure_ascii=False)
+                        commenter_message_created_time = json.dumps(comment['created_time'], ensure_ascii=False)
+
+                        # self.writer.writerow([unicode(s).encode("utf-8") for s in row])
+                        print 'commenter_name: %s' % commenter_name
+                        # csv_writer.writerow({'commenter_id': '', 'commenter_name': commenter_name, 'commenter_message': '', 'commenter_message_created_time': '', 'replier_id': '', 'replier_name': '', 'replier_message': '', 'replier_message_created_time': ''})
+                        data.append(('', commenter_name, '', '', '', '', '', ''))
 
 
-                    #check whether has replies or not
-                    # 查看留言是否有回覆
-                    r = graph.get_connections(id=comment['id'], connection_name="comments")
+                        #check whether has replies or not
+                        # 查看留言是否有回覆
+                        r = graph.get_connections(id=comment['id'], connection_name="comments")
 
-                    for reply in r['data']:
-                        # 名稱/id/內容/時間
-                        replier_name = json.dumps(reply['from']['name'])
-                        replier_id = json.dumps(reply['from']['id'])
-                        replier_message = json.dumps(reply['message'])
-                        replier_message_created_time = json.dumps(reply['created_time'])
-                        csv_writer.writerow({'replier_name': replier_name})
-                        # comment.update({"replies" : r})
+                        # try:
+                        #     r = graph.get_connections(id=comment['id'], connection_name="comments")
+                        # except:
+                        #     get_comments_successfully = False
+                        #     while not get_comments_successfully:
+                        #         r = graph.get_connections(id=comment['id'], connection_name="comments")
+                        #         if 'data' in r:
+                        #             get_comments_successfully = True
 
-                        print "=============== reply data ============="
-                        print json.loads(json.dumps(reply, ensure_ascii=False), encoding="utf-8")
-                        print "=============== reply data ============="
+                        print "Get replies data:"
+                        print json.dumps(r, ensure_ascii=False)
 
-                if next_comments_url == '':
-                    break
-            # print "Did not got comment!"
-        # print json.dumps(ele, ensure_ascii=False)
+                        next_replies_url = ''
+                        if 'paging' in r:
+                            if 'next' in r['paging']:
+                                next_replies_url = r['paging']['next']
 
-        # print '--------------------------------------'
-    # with io.open(file_name, 'w', encoding='UTF-8') as file:
-        # file.write(json.dumps(feeds, ensure_ascii=False))
+                        is_first_round_in_replies = True
 
+                        while True:
+                            if is_first_round_in_replies:
+                                replies = r['data']
+                                is_first_round_in_replies = False
+                            else:
+                                if next_replies_url != '':
+                                    r = requests.get(next_replies_url)
+                                    json_obj = json.loads(r.text)
+                                    replies = json_obj['data']
+                                    next_replies_url = ''
+                                    if 'paging' in json_obj:
+                                        if 'next' in json_obj['paging']:
+                                            next_replies_url = json_obj['paging']['next']
+
+                            for reply in replies:
+                                # 名稱/id/內容/時間
+                                replier_name = json.dumps(reply['from']['name'], ensure_ascii=False)
+                                replier_id = json.dumps(reply['from']['id'], ensure_ascii=False)
+                                replier_message = json.dumps(reply['message'], ensure_ascii=False)
+                                replier_message_created_time = json.dumps(reply['created_time'], ensure_ascii=False)
+                                # csv_writer.writerow({'replier_name': replier_name})
+                                data.append(('', '', '', '', '', replier_name, '', ''))
+
+                                print "=============== reply data ============="
+                                print json.dumps(reply, ensure_ascii=False)
+                                print "=============== reply data ============="
+
+                            if next_replies_url == '':
+                                break
+
+                    # 如果沒有下一頁的 comment， 就 break while 迴圈
+                    if next_comments_url == '':
+                        break
+
+                # print "Did not got comment!"
+            # print json.dumps(ele, ensure_ascii=False)
+
+            # print '--------------------------------------'
+        # with io.open(file_name, 'w', encoding='UTF-8') as file:
+            # file.write(json.dumps(feeds, ensure_ascii=False))
+
+            data = tablib.Dataset(*data, headers=headers)
+            csv_file.write(data.xls)
+            csv_file.close()
+
+        next_url = ""
+        if 'paging' in feeds:
+            if 'next' in feeds['paging']:
+                next_url = feeds['paging']['next']
+
+        print "next url (feeds): %s" % next_url
+
+        if next_url != '':
+            r = requests.get(next_url)
+            get_comments(json.loads(r.text), "")
+        else:
+            print "end!!!!"
+    except:
+        data = tablib.Dataset(*data, headers=headers)
+        csv_file.write(data.xls)
         csv_file.close()
-
-    next_url = ""
-    if 'paging' in feeds:
-        if 'next' in feeds['paging']:
-            next_url = feeds['paging']['next']
-
-    print "next url (feeds): %s" % next_url
-
-    if next_url != '':
-        r = requests.get(next_url)
-        get_comments(json.loads(r.text), "")
-    else:
-        print "end!!!!"
 
 
 
 get_comments(_feeds, "")
 
 
-
-# r = requests.get(next_url)
-# with io.open('next_result.json', 'w', encoding='UTF-8') as file:
-    # file.write(json.dumps(json.loads(r.text), ensure_ascii=False))
-
-
-# with io.open('result.json', 'w', encoding='UTF-8') as file:
-    # file.write(json.dumps(feeds, ensure_ascii=False))
-
 sys.exit(0)
-
-# print feeds['data']
-
-# with io.open('result.json', 'w', encoding='UTF-8') as file:
-    # file.write(json.dumps(feeds, ensure_ascii=False))
-
-# comment_id = '874027102667527_874374989299405'
-# comment_id = '877895362280701_877916398945264'
-# r = graph.get_connections(id=comment_id, connection_name="comments")
-# print json.dumps(r, ensure_ascii=False)
-
-# print s
-
-def save_comment_and_replies(comment, csv_writer):
-    pass
 
